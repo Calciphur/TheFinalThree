4a675f930a47c8f44259a26cd0198198276f748a
Dane <cdcravens16@gmail.com>
2017-01-19 16:39:13 -0500

    add insert_hand method to player.rb and appropriate tests

--- player.rb
+++ player.rb
@@ -14,13 +14,19 @@ class Player
     end
   end
 
+  def insert_hand(deck, num=@hand.length)
+    num.times do
+      deck.deck_list.push(@hand.pop)
+    end
+  end
+
   def view_hand
     if @hand.empty?
       'empty'
     else
       (@hand.length).times do |i|
         puts "#{@hand[i].print}"
-      end 
+      end
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -22,6 +22,26 @@ describe 'Player' do
     end
   end
 
+  describe 'insert_hand' do
+    it "should remove all cards from 'hand' when no num param passed" do
+      player = Player.new(1)
+      deck = Deck.new 'standard', 52
+      player.draw(deck, 3)
+      player.insert_hand(deck)
+      expect(player.hand.length).to eq(0)
+      expect(deck.count_deck).to eq(52)
+    end
+
+    it "should remove specified # cards from hand when num param passed" do
+      player = Player.new(1)
+      deck = Deck.new 'standard', 52
+      player.draw(deck, 3)
+      player.insert_hand(deck, 1)
+      expect(player.hand.length).to eq(2)
+      expect(deck.count_deck).to eq(50)
+    end
+  end
+
   describe 'view_hand' do
 
     it "should return 'empty' if the hand is empty" do
@@ -31,6 +51,6 @@ describe 'Player' do
 
     # can't get test to work to check whether or not the view_hand method
     # is printing formatted contents of the hand array :(
-    
+
   end
 end
